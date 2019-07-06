94c75b28f710dd8932154832245f36869e3771f9
Krembo <testmail@web.de>
2019-07-06 16:19:50 +0200

    update

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 var express = require("express");
 var app = express();
+const path = require('path');
 //var cors = require("cors");
 var bodyParser = require("body-parser");
 
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/ui5con");
+const dbURL = "mongodb://heroku_gkktb1k2:qn6smi3o6nt6vcjc37u88t5im@ds247637.mlab.com:47637/heroku_gkktb1k2";
+let port = process.env.PORT;
+var db;
+var dbString = "mongodb://localhost/ui5con";
+
+if (port == null || port == "") {
+  port = 3000;
+}
+else {
+  dbString = dbURL;
+}
+
+mongoose.connect(dbString);
 //app.use(cors());
 app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: true }))
 var Product = mongoose.model("products", {
   name: String
 });
-
 /*
 var product = new Product({name:"UI5Con"});
 product.save(function(err){
@@ -21,6 +34,15 @@ product.save(function(err){
   }
 });
 */
+
+
+app.get('/', (req, res) => {
+   const index = path.join(__dirname, 'index.html');
+   res.sendFile(index);
+  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+});
+
 
 app.get("/products", function(req, res){
   Product.find(function(err, products){
